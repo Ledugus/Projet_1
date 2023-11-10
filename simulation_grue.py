ebd8cb74c2c854b4268838193d3dbea0eeb47545
Ledugus <aaugustinwwezel@gmail.com>
2023-11-10 15:56:18 +0100

    Amélioration de l'affichage du plot simulation_grue.py

--- simulation_grue.py
+++ simulation_grue.py
@@ -3,36 +3,45 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-### Constantes
+# Constantes
 
 g = 9.81  # gravitation [m/s**2]
 
-### Paramètres du système
-m_charge = 0.7  # mass de la charge déplacée [kg]
-h_charge = 0.1 # hauteur de la charge au bas de la barge
+# Paramètres du système
+
+m_charge = 0.2  # mass de la charge déplacée [kg]
+h_charge = 0.3  # hauteur de la charge au bas de la barge
 d = 2  # distance de la charge au centre de la barge [m]
 
-m_caisse = 0
-h_caisse = 0.1
+m_caisse = 0  # masse du chargement sur la barge
+h_caisse = 0.1  # hauteur du centre de gravité du chargement sur la barge
 
 l_barge = 0.6  # longueur de la barge [m]
 h_barge = 0.08  # hauteur de la barge [m]
-vol_barge = h_barge * l_barge ** 2 # volume de la barge [m³]
-rho_barge = 1000/8 # masse volumique de la barge [kg/m³]
-m_barge = rho_barge * vol_barge # masse de la barge [kg]
+vol_barge = h_barge * l_barge ** 2  # volume de la barge [m³]
+rho_barge = 10 / vol_barge  # masse volumique de la barge [kg/m³]
+m_barge = rho_barge * vol_barge  # masse de la barge [kg]
 
-D = 0.3 # coefficient de frottement visqueux
+D = 0.6  # coefficient de frottement visqueux
+
 
 def calculate_moment_inertie():
     moment_barge = (m_barge * (l_barge ** 2 + h_barge ** 2)) / 12
-    moment_caisse = m_caisse * (h_caisse-h_c)**2
-    moment_charge = m_charge * (d**2+(h_charge-h_c)**2)
-    moment_inertie = moment_barge + moment_caisse + moment_charge
-    return moment_inertie
+    moment_caisse = m_caisse * (h_caisse - h_c) ** 2
+    moment_charge = m_charge * (d ** 2 + (h_charge - h_c) ** 2)
+    return moment_barge + moment_caisse + moment_charge
+
 
 def calculate_stable_angle():
-    return math.atan((m_charge*d)/(m_tot * ((l_barge**2)/(12*h_c)-h_c/2-(z_g-h_c))))
+    return math.atan((m_charge * d) / (m_tot * ((l_barge ** 2) / (12 * h_c) - h_c / 2 - (z_g - h_c))))
 
+
+def get_x_c(theta):
+    return (l_barge ** 2 / (12 * h_c) - (h_c / 2)) * math.sin(theta)
+
+
+def get_x_g(theta):
+    return (math.sin(theta) * (z_g - h_c)) + ((math.cos(theta) * m_charge * d) / m_tot)
 
 # Grandeurs constantes calculées
 m_tot = m_charge + m_barge + m_caisse
@@ -41,14 +50,15 @@ h_c = m_tot / ((l_barge ** 2) * 1000)
 z_g = (m_barge * (h_barge / 2) + m_charge * h_charge + m_caisse * h_caisse / 2) / m_tot
 moment_inertie = calculate_moment_inertie()
 angle_stable = calculate_stable_angle()
-angle_submersion = 0
-angle_soulevement = 0
+angle_submersion = math.atan(2 * (h_barge - h_c) / l_barge)
+angle_soulevement = math.atan(2 * h_c / l_barge)
+print(moment_inertie)
 
-### Paramètres de la simulation
+# Paramètres de la simulation
 
 step = 0.001  # pas (dt) [s]
-end = 20.0  # durée [s]
-theta_0 = 0  # angle d'inclinaison du départ
+end = 10.0  # durée [s]
+theta_0 = 0  # angle d'inclinaison du départ [rad]
 
 t = np.arange(0, end, step)
 x_g = np.empty_like(t)
@@ -59,7 +69,7 @@ x_c = np.empty_like(t)
 
 
 def get_x_c(theta):
-    return (l_barge**2 / (12 * h_c) - (h_c / 2)) * math.sin(theta)
+    return (l_barge ** 2 / (12 * h_c) - (h_c / 2)) * math.sin(theta)
 
 
 def get_x_g(theta):
@@ -86,7 +96,7 @@ def simulation():
         couple_archimede = -f_poussee_gravite * x_c[i]
         couple_tot = couple_gravite + couple_archimede
 
-        # calcul accélération, vitesse, angle
+        # calcul accélération et vitesse angulaire, angle
         a_theta[i] = (couple_tot / moment_inertie) - D * v_theta[i]
         v_theta[i + 1] = v_theta[i] + a_theta[i] * dt
         theta[i + 1] = theta[i] + v_theta[i] * dt
@@ -95,27 +105,32 @@ def simulation():
 
 def graphiques():
     plt.figure(1)
-    plt.subplot(3, 1, 1)
+    # plt.subplot(3, 1, 1)
     plt.plot(t, theta, label="Angle de la plateforme")
+    plt.xlabel("Temps (s)")
+    plt.ylabel("Angle (rad)")
     # ligne de stabilité
-    plt.axhline(y=angle_stable, label="Stabilité théorique", color="blue") 
+    plt.axhline(y=angle_stable, label="Stabilité théorique", color="blue", linestyle="dotted")
     # ligne de submersion
-    plt.axhline(y=angle_submersion, color="red") 
-    plt.axhline(y=-angle_submersion, color="red")
+    plt.axhline(y=angle_submersion, label="Submersion", xmin=0, xmax=end, color="black", linestyle="dotted")
+    plt.axhline(y=-angle_submersion, xmin=0, xmax=end, color="black", linestyle="dotted")
     # ligne de soulevement
-    plt.axhline(y=angle_soulevement, color="orange")
-    plt.axhline(y=-angle_soulevement, color="orange")
+    plt.axhline(y=angle_soulevement, label="Soulèvement", xmin=0, xmax=end, color="red", linestyle="dotted")
+    plt.axhline(y=-angle_soulevement, xmin=0, xmax=end, color="red", linestyle="dotted")
     plt.legend()
 
-    plt.subplot(3, 1, 2)
+    """plt.subplot(3, 1, 2)
     plt.plot(t, v_theta, label="Vitesse angulaire de la plateforme")
     plt.legend()
     plt.subplot(3, 1, 3)
     plt.plot(t, a_theta, label="Accélération angulaire de la plateforme")
-    plt.legend()
+    plt.legend()"""
+
+    plt.savefig("simulation.png")
     plt.show()
 
-### programme principal
+
+# Programme principal
 if __name__ == "__main__":
     simulation()
     graphiques()

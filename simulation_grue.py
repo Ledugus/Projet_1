6cb053da7273b9afb55e3b6d8af45d956d68fd94
chromebook <aaugustinwwezel@gmail.com>
2023-12-13 19:08:55 +0100

    Refaire les graphiques

--- simulation_grue.py
+++ simulation_grue.py
@@ -4,51 +4,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Constantes
-
 g = 9.81  # gravitation [m/s**2]
 
 # Paramètres du système
-
 m_charge = 0.2  # mass de la charge déplacée [kg]
 h_charge = 0.3  # hauteur de la charge au bas de la barge [m]
 d = 2  # distance de la charge au centre de la barge [m]
 
 m_grue = 4  # masse du chargement sur la barge [kg]
-h_grue = 0.3  # hauteur du centre de gravité du chargement sur la barge [m]
+h_grue = 0.5  # hauteur du centre de gravité du chargement sur la barge [m]
 
 l = 0.6  # longueur de la barge [m]
 h1 = 0.08  # hauteur de la barge [m]
-m_barge = 2    # masse de la barge [kg]
+m1 = 5  # masse de la barge [kg]
 
 D = 0.6  # coefficient de frottement visqueux
 
 
 def calculate_moment_inertie():
-    moment_barge = (m_barge * (l ** 2 + h1 ** 2)) / 12
+    """Retourne le moment d'inertie de la plateforme"""
+    # moment d'un parallélipipède (plateforme)
+    moment_barge = (m1 * (l ** 2 + h1 ** 2)) / 12
+    # moment d'une masse ponctuelle (grue)
     moment_caisse = m_grue * (h_grue - h_c) ** 2
+    # moment d'une masse ponctuelle (charge)
     moment_charge = m_charge * (d ** 2 + (h_charge - h_c) ** 2)
-    return moment_barge + moment_caisse + moment_charge
+
+    return moment_barge + moment_caisse + moment_charge  # somme des moments
 
 
 def calculate_stable_angle(distance: float, masse_charge: float) -> float:
+    """Retourne l'angle de stabilité théorique"""
     return math.atan((masse_charge * distance) / (m_tot * ((l ** 2) / (12 * h_c) + h_c / 2 - (z_g))))
 
 
 def d_by_time(time: float, time_max: float) -> float:
+    """Retourne la distance de la charge en fonction du temps (charge variable)"""
     return min(d*time/time_max + 0.3, d)
 
 
 # Autres paramètres calculés
 
-m_tot = m_charge + m_barge + m_grue
+m_tot = m_charge + m1 + m_grue  # masse totale [kg]
+# Intensité des forces de poussée et de gravité [N]
 f_poussee_gravite = m_tot * g
-h_c = m_tot / ((l ** 2) * 1000)
-z_g = (m_barge * (h1 / 2) + m_charge *
-       h_charge + m_grue * h_grue / 2) / m_tot
+h_c = m_tot / ((l ** 2) * 1000)  # enfoncement de la plateforme [m]
+z_g = (m1 * ((h1 / 2)-h_c) + m_charge *
+       h_charge + m_grue * h_grue / 2) / m_tot  # hauteur du centre de gravité du système [m]
 moment_inertie = calculate_moment_inertie()
 angle_stable = calculate_stable_angle(d, m_charge)
-angle_submersion = math.atan(2 * (h1 - h_c) / l)
-angle_soulevement = math.atan(2 * h_c / l)
+angle_submersion = math.atan(2 * (h1 - h_c) / l)  # angle de submersion
+angle_soulevement = math.atan(2 * h_c / l)  # angle de soulèvement
 
 # Paramètres de la simulation
 
@@ -56,6 +62,7 @@ step = 0.001  # pas (dt) [s]
 end = 20.0  # durée [s]
 theta_0 = 0  # angle d'inclinaison du départ [rad]
 
+# Initialisation des listes
 t = np.arange(0, end, step)
 x_g = np.empty_like(t)
 theta = np.empty_like(t)
@@ -65,26 +72,29 @@ x_c = np.empty_like(t)
 
 
 def get_x_c(theta: float) -> float:
+    """Retourne la position du centre de poussée"""
     return (l ** 2 / (12 * h_c) - (h_c / 2)) * math.sin(theta)
 
 
 def get_x_g(theta: float, d: float) -> float:
-    return (math.sin(theta) * (z_g - h_c)) + ((math.cos(theta) * m_charge * d) / m_tot)
+    """Retourne la position du centre de gravité"""
+    return (math.sin(theta) * (z_g)) + ((math.cos(theta) * m_charge * d) / m_tot)
 
 
 def simulation():
     """
     pre: 
     post: exécute une simulation jusqu'à t=end par pas de dt=step.
-          Remplit les listes theta, v_theta, a_theta des angles, vitesses et accélérations angulaires.
+                      Remplit les listes theta, v_theta, a_theta des angles, vitesses et accélérations angulaires.
     """
     # conditions initiales
     theta[0] = theta_0
 
     for i in range(len(t) - 1):
         dt = step
+
         # calcul des points d'application des forces
-        x_g[i] = get_x_g(theta[i], d_by_time(i*dt, end/4))
+        x_g[i] = get_x_g(theta[i], d_by_time(dt*i, 10))
         x_c[i] = get_x_c(theta[i])
 
         # calcul des couples
@@ -139,7 +149,7 @@ def diagramme_des_phases():
     plt.ylabel("Vitesse (rad/s)")
     plt.legend()
     plt.plot(angle_stable, 0, 'bo')
-    plt.savefig("diagramme_phases.png")
+    plt.savefig("diagramme_phases_charge_variable.png")
     plt.show()
 
 
@@ -174,3 +184,4 @@ def inclinaison_distance_masse():
 if __name__ == "__main__":
     simulation()
     graphique_angle_temps(draw_lines=False)
+    diagramme_des_phases()

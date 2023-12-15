ac83024d72b2ba298b4b53e8b9274b51bc2615d4
chromebook <aaugustinwwezel@gmail.com>
2023-12-15 21:51:47 +0100

    Tracker

--- simulation_grue.py
+++ simulation_grue.py
@@ -18,7 +18,7 @@ l = 0.6  # longueur de la barge [m]
 h1 = 0.08  # hauteur de la barge [m]
 m1 = 5  # masse de la barge [kg]
 
-D = 0.6  # coefficient de frottement visqueux
+D = 0.4  # coefficient de frottement visqueux
 
 
 def calculate_moment_inertie():
@@ -56,20 +56,6 @@ angle_stable = calculate_stable_angle(d_max, m_charge)
 angle_submersion = math.atan(2 * (h1 - h_c) / l)  # angle de submersion
 angle_soulevement = math.atan(2 * h_c / l)  # angle de soulèvement
 
-# Paramètres de la simulation
-
-step = 0.001  # pas (dt) [s]
-end = 20.0  # durée [s]
-theta_0 = 0  # angle d'inclinaison du départ [rad]
-
-# Initialisation des listes
-t = np.arange(0, end, step)
-x_g = np.empty_like(t)
-theta = np.empty_like(t)
-v_theta = np.empty_like(t)
-a_theta = np.empty_like(t)
-x_c = np.empty_like(t)
-
 
 def get_x_c(theta: float) -> float:
     """Retourne la position du centre de poussée"""
@@ -80,16 +66,28 @@ def get_x_g(theta: float, d: float) -> float:
     """Retourne la position du centre de gravité"""
     return (math.sin(theta) * (z_g)) + ((math.cos(theta) * m_charge * d) / m_tot)
 
+# Paramètres de la simulation
+
+
+step = 0.001  # pas (dt) [s]
+end = 20.0  # durée [s]
+theta_0 = 0  # angle d'inclinaison du départ [rad]
+
 
 def simulation(charge_variable=False):
     """
     pre: 
     post: exécute une simulation jusqu'à t=end par pas de dt=step.
-                      Remplit les listes theta, v_theta, a_theta des angles, vitesses et accélérations angulaires.
+                                      Remplit les listes theta, v_theta, a_theta des angles, vitesses et accélérations angulaires.
     """
     # conditions initiales
+    t = np.arange(0, end, step)
+    x_g = np.empty_like(t)
+    theta = np.empty_like(t)
+    v_theta = np.empty_like(t)
+    a_theta = np.empty_like(t)
+    x_c = np.empty_like(t)
     theta[0] = theta_0
-
     for i in range(len(t) - 1):
         dt = step
 
@@ -108,9 +106,57 @@ def simulation(charge_variable=False):
         v_theta[i + 1] = v_theta[i] + a_theta[i] * dt
         theta[i + 1] = theta[i] + v_theta[i] * dt
         a_theta[i + 1] = a_theta[i]
+    return t, theta, v_theta, a_theta
 
 
-def graphique_angle_temps(draw_lines=True):
+def get_donnees_tracker(filename):
+    """Retourne les données Tracker du fichier spécifié"""
+    f = open(filename)
+    data = f.read()
+
+    # Extraire les données en listes
+    temps, position_y = [], []
+    for ligne in data.split('\n'):
+        if ligne:
+            t, y = map(float, ligne.replace(',', '.').split())
+            temps.append(t)
+            position_y.append(y)
+    # Convertir les listes en tableaux NumPy pour une manipulation plus aisée
+    temps = np.array(temps)
+    position_y = np.array(position_y)
+
+    # Effectuez une régression polynomiale d'ordre 2 (ajustez cet ordre selon vos besoins)
+    coefficients = np.polyfit(temps, position_y, len(temps)-2)
+
+    # Créez une fonction polynomiale à partir des coefficients
+    polynome = np.poly1d(coefficients)
+
+    # Générez des valeurs de temps pour la courbe d'approximation
+    temps_approx = np.linspace(temps.min(), temps.max(), 100)
+
+    # Calculez les valeurs de position y correspondantes à la courbe d'approximation
+    position_y_approx = polynome(temps_approx)-0.422
+    cos_angle = position_y_approx/0.3
+    angle_approx = np.arccos(np.clip(cos_angle, -1.0, 1.0))-1
+    angles_en_degres = (np.degrees(angle_approx) - 90)
+    return temps_approx, angle_approx
+
+
+def get_periode(array):
+    array = array[400:]
+    min_array = min(array)
+    max_array = max(array)
+    return abs(np.where(array == max_array)[0][0]-np.where(array == min_array)[0][0])
+
+
+def get_amp(array):
+    min_array = min(array)
+    max_array = max(array)
+    return max_array-min_array
+
+
+def graphique_angle_temps(title="angle_temps_charge_variable_simple.png", draw_lines=True, donnees_tracker=(), charge_variable=False):
+    t, theta, v_theta, a_theta = simulation(charge_variable=charge_variable)
     plt.figure(1)
     if not draw_lines:
         plt.subplot(3, 1, 1)
@@ -120,6 +166,9 @@ def graphique_angle_temps(draw_lines=True):
     # ligne de stabilité
     plt.axhline(y=angle_stable, label="Stabilité théorique",
                 color="blue", linestyle="dotted")
+    if donnees_tracker and draw_lines:
+        plt.plot(donnees_tracker[0], donnees_tracker[1],
+                 label='Angle approximé en rad')
     if draw_lines:
         # ligne de submersion
         plt.axhline(y=angle_submersion, label="Submersion", xmin=0,
@@ -139,11 +188,12 @@ def graphique_angle_temps(draw_lines=True):
         plt.subplot(3, 1, 3)
         plt.plot(t, a_theta, label="Accélération angulaire de la plateforme")
         plt.legend()
-    plt.savefig("angle_temps_charge_variable_simple.png")
+    plt.savefig(title)
     plt.show()
 
 
-def diagramme_des_phases():
+def diagramme_des_phases(charge_variable=False):
+    t, theta, v_theta, a_theta = simulation(charge_variable=charge_variable)
     plt.figure(1)
     plt.plot(theta, v_theta, label="Phase du système")
     plt.xlabel("Angle (rad)")
@@ -183,5 +233,12 @@ def inclinaison_distance_masse():
 
 # Programme principal
 if __name__ == "__main__":
-    simulation(charge_variable=True)
-    graphique_angle_temps(draw_lines=True)
+    temps_approx, angle_approx = get_donnees_tracker("tracker.txt")
+    print(get_amp(angle_approx))
+    for x in range(13, 14):
+        D = x/10 + 0.1
+        t, theta, v_theta, a_theta = simulation(charge_variable=False)
+        print(D, get_periode(theta)*step, get_amp(theta))
+
+        graphique_angle_temps(title=str(x),
+                              draw_lines=True, donnees_tracker=(temps_approx, angle_approx-angle_approx[0]))
